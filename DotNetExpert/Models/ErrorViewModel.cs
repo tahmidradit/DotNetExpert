27ef116974cb20e8d7d8644864f642ba115e104b
tahmidradit <tahmid.radit@outlook.com>
2022-01-10 23:08:16 +0600

    Initial commit

---
+++ DotNetExpert/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DotNetExpert.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
